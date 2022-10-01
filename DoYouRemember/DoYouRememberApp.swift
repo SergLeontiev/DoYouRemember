da8b7b2ce624c36a4e0f1ad1bf487c1321b9ec8e
SergLeontiev <translatertrans@gmail.com>
2022-10-01 10:42:11 +0200

    Initial Commit

---
+++ DoYouRemember/DoYouRememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoYouRememberApp.swift
+//  DoYouRemember
+//
+//  Created by Sergey Leontiev on 01.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct DoYouRememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
