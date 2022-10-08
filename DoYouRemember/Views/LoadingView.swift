b8545a64efa1c74d8a80f76f8284c20e3d236f0c
SergLeontiev <translatertrans@gmail.com>
2022-10-08 16:56:09 +0200

    Add LoadingView

---
+++ DoYouRemember/Views/LoadingView.swift
@@ -0,0 +1,48 @@
+//
+//  LoadingView.swift
+//  DoYouRemember
+//
+//  Created by Sergey Leontiev on 08.10.2022.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    @State private var isLoading = false
+    private let heights: [CGFloat] = [8, 5, 14, 31, 13, 32, 9]
+    
+    private func getLoadingAnimation(index: Int) -> Animation {
+        Animation.default
+            .repeatForever(autoreverses: false)
+            .speed(0.4)
+            .delay(0.07 * Double(index))
+    }
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 19) {
+            
+            HStack(spacing: 2) {
+                ForEach(Array(heights.enumerated()), id: \.offset) { item in
+                    RoundedRectangle(cornerRadius: 0)
+                        .foregroundColor(isLoading ? .gray : .black)
+                        .frame(width: 2, height: item.element)
+                        .animation(getLoadingAnimation(index: item.offset))
+                }
+            }
+            .padding(.horizontal, 3)
+            
+            Text("Searching for translation...")
+                .font(.system(size: 12, weight: .regular))
+                .foregroundColor(.gray)
+        }
+        .onAppear() {
+            isLoading.toggle()
+        }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
